c9f47cfa7d3c27e32f38a873ed5bc844500d6c80
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2021-01-28 22:22:40 -0300

    Add Carousel and images

---
+++ react-login/src/components/Carousel.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Carousel from 'react-bootstrap/Carousel'
+
+const Slider = () => {
+    return (
+      <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="https://wallpapercave.com/wp/wp5437792.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src="https://wallpapercave.com/wp/wp5437792.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://wallpapercave.com/wp/wp5437792.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    )
+}
+
+export default Slider;
